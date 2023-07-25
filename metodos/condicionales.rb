5b2354015ae090dc0f6a0c3f7766f5137c22cb2d
Isaac Delgado <113700281+Illu13@users.noreply.github.com>
2023-07-25 21:50:03 +0200

    Tutorial keeps going

---
+++ metodos/condicionales.rb
@@ -0,0 +1,9 @@
+valor = true
+valor = 0
+valor = nil
+
+if valor == true
+    puts "faston"
+else
+    puts "sexton"
+end
--- metodos/metodo.rb
+++ metodos/metodo.rb
@@ -6,5 +6,19 @@ def despedirse
     puts"Adios mundo"
 end
 
+alias s  saludar
+
+=begin
+Podemos tener dos métodos con el mismo nombre con el alias.    
+Interesante a la hora de tener dos métodos con una función bastante similar.
+=end
+
+def saludar
+    puts'HOLAAA'
+end
+
+
+
+s
 saludar
 despedirse
