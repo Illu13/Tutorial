107036e91f90c5cf09abedebc6dd2ea41f077695
Isaac Delgado <113700281+Illu13@users.noreply.github.com>
2023-07-25 21:50:16 +0200

    and it keeps going

--- metodos/metodoargumentos.rb
+++ metodos/metodoargumentos.rb
@@ -11,11 +11,17 @@ def sumar2(num1, num2)
    puts resultado
 end
 
+def restar(num1, num2)
+    resultado = num1 - num2
+    return resultado
+ end
+
+
 
 def long (nombre)
     nombre.length
 end
-
+puts "El resultado es: #{restar(33,22)}"
 puts long(nombre)
 puts sumar(n1, n2)
 sumar2(n1,n2)
